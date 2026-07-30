77f65933765fcc9335161631dff0b7755f6370ef
brendaalcantara-cell <brenda.alcantara@escola.pr.gov.br>
2026-07-30 15:14:16 +0000

    jogo  da forca

---
+++ jogodaforca.py
@@ -0,0 +1,15 @@
+# jogo-da-forca
+print("****************************")
+print("Bem vindo ao jogo da força")
+print("****************************")
+
+palavraecreta= "forte"
+letrasaertadas=["_","_","_","_","_","_"]
+
+enforcou= False
+acertou = False
+
+whille(not enforcou and not acertou):
+chute = input("Digite uma letra ")
+chute = chute.strip()
+
